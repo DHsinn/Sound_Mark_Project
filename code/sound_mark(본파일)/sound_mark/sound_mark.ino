0d86b90fbaddd2f5b05fd37e3a6be240445eb2d5
Purposeful sin <88013497+DHsinn@users.noreply.github.com>
2023-04-04 13:46:23 +0900

    230404
    비콘에 음악재생 추가

--- code/sound_mark(본파일)/sound_mark/sound_mark.ino
+++ code/sound_mark(본파일)/sound_mark/sound_mark.ino
@@ -15,7 +15,7 @@ int nds[] = {4,4,4,4,4,4,2,4,4,4,4,1,4,4,4,4,4,4,2,4,4,4,4};
 //오디오 모듈
 #include <SoftwareSerial.h>
 #include <MP3Player_KT403A.h>
-SoftwareSerial mp3(10, 9); //TX, RX
+SoftwareSerial mp3(17, 16); //TX, RX
 
 
 #include "sys/time.h"
@@ -102,12 +102,12 @@ void setup() {
     noTone(speakerpin);
   }  //학교종이 땡땡땡 출력
   */
-  mp3.begin(115200);
-  Serial.begin(115200);
-  delay(1000);
-
-  SelectPlayerDevice(0x01);
-  SetVolume(0x0E);
+  mp3.begin(9600);
+    Serial.begin(9600); 
+    delay(100);
+    
+    SelectPlayerDevice(0x02);       // Select SD card as the player device.
+    SetVolume(0x0E);                // Set the volume, the range is 0x00 to 0x1E.
 }
 
 void loop() {
@@ -124,6 +124,55 @@ void loop() {
       tone(speakerpin,0,0);
     }
   }*/
+
+// mp3 파일 재생 명령어
+  char recvChar = 0;
+    while(Serial.available() > 0)
+    {
+        recvChar = Serial.read();
+    }
+    Serial.print("Send: ");
+    Serial.println( recvChar );
+    
+    switch (recvChar)
+    {
+        case '1':
+            SpecifyMusicPlay(1);
+            Serial.println("Specify the music index to play");
+            break;
+        case '2':
+            PlayPause();
+            Serial.println("Pause the MP3 player");
+            break;
+        case '3':
+            PlayResume();
+            Serial.println("Resume the MP3 player");
+            break;
+        case '4':
+            PlayNext();
+            Serial.println("Play the next song");
+            break;
+        case '5':
+            PlayPrevious();
+            Serial.println("Play the previous song");
+            break;
+        case '6':
+            PlayLoop();
+            Serial.println("Play loop for all the songs");
+            break;
+        case '7':
+            IncreaseVolume();
+            Serial.println("Increase volume");
+            break;
+        case '8':
+            DecreaseVolume();
+            Serial.println("Decrease volume");
+            break;
+        default:
+            break;
+    }
+    
+    delay(1000);
 
   /*
   -사용자 입장-

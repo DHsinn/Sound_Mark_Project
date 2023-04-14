2b9d828f71c976b6479677043d65ff898f9a8ccb
Purposeful sin <88013497+DHsinn@users.noreply.github.com>
2023-04-14 18:01:36 +0900

    230414
    at 커맨드 성공!!!..ㅠ

--- code/sirial_connection/sirial_connection.ino
+++ code/sirial_connection/sirial_connection.ino
@@ -1,17 +1,22 @@
-#include <HardwareSerial.h>
+#include <SoftwareSerial.h>
 
-void setup() {
-  // 시리얼 통신 초기화
+SoftwareSerial BTSerial(3, 1); // SoftwareSerial(RX, TX)
+
+void setup()
+{
   Serial.begin(115200);
+  Serial.println("Hello!");
+  // set the data rate for the BT port
+  BTSerial.begin(115200);
 }
 
-void loop() {
-  // 입력한 데이터가 있을 경우
-  if (Serial.available()) {
-    // 시리얼 포트로부터 데이터를 읽어서 변수에 저장
-    char incomingData = Serial.read();
-
-    // 읽은 데이터를 시리얼 포트로 다시 출력
-    Serial.write(incomingData);
+void loop(){
+  while (BTSerial.available()){ // if BT sends something
+    byte data = BTSerial.read();
+    Serial.write(data); // write it to serial(serial monitor)
+  }
+  while (Serial.available()){ // if Serial has input(from serial monitor)
+    byte data = Serial.read();
+    BTSerial.write(data); // write it to BT
   }
 }

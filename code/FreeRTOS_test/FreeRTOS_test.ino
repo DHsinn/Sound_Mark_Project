824ebb7d957d8ce0f9e2fd80fbd98787f0625f39
Purposeful sin <88013497+DHsinn@users.noreply.github.com>
2023-06-28 18:01:38 +0900

    230628

--- code/FreeRTOS_test/FreeRTOS_test.ino
+++ code/FreeRTOS_test/FreeRTOS_test.ino
@@ -7,7 +7,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-// iBeacon UUID
+// UUID
 #define BEACON_UUID "8ec76ea3-6668-48da-9866-75be8bc86f4d"
 #define TEST_BEACON_UUID "39ED98FF-2900-441A-802F-9C398FC199D2"
 
@@ -15,43 +15,147 @@
 #define SCAN_INTERVAL 100
 #define SCAN_PERIOD 10000
 
+//오디오
+#include "SoftwareSerial.h"
+#include "MP3Player_KT403A.h"
+SoftwareSerial mp3(17, 16); //TX, RX  (GIOP 번호)
+
+bool playsong = true;
+
+BLEScan* pBLEScan;
+BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+BLEBeacon oBeacon = BLEBeacon();
+BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
+
+SemaphoreHandle_t semaphore;
 TaskHandle_t scanTaskHandle;
 TaskHandle_t beaconTaskHandle;
 
-void scanTask(void *pvParameters);
-void beaconTask(void *pvParameters);
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
+  public:
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    std::string manufacturerData = advertisedDevice.getManufacturerData();
+    
+    // Check if the advertised device has manufacturer data and matches the desired ID
+    if (advertisedDevice.haveManufacturerData() && manufacturerData.length() == 25 &&
+        manufacturerData[0] == 0x4c && manufacturerData[1] == 0x00 &&
+        manufacturerData[2] == 0x02 && manufacturerData[3] == 0x15 &&
+        manufacturerData[4] == 0x39 && manufacturerData[5] == 0xed &&
+        manufacturerData[6] == 0x98 && manufacturerData[7] == 0xff &&
+        manufacturerData[8] == 0x29 && manufacturerData[9] == 0x00) {
+      
+    }
+  }
+};
+
+MyAdvertisedDeviceCallbacks callbacks;
+
+void scanTask(void* pvParameters){
+  bool isPlaying = false;
+  unsigned long startTime = 0;
+
+  for (;;) {
+    if (xSemaphoreTake(semaphore, portMAX_DELAY) == pdTRUE) {
+      BLEScanResults foundDevices = pBLEScan->start(1, false);
+      for (int i = 0; i < foundDevices.getCount(); i++) {
+        BLEAdvertisedDevice advertisedDevice = foundDevices.getDevice(i);
+        callbacks.onResult(advertisedDevice);
+
+        std::string manufacturerData = advertisedDevice.getManufacturerData();
+        if (manufacturerData.length() >= 24) {
+          //신호값 받기
+          uint16_t major = manufacturerData[20] << 8 | manufacturerData[21];
+          uint16_t minor = manufacturerData[22] << 8 | manufacturerData[23];
+          
+          if (major == (1<<8) && minor == 1) {
+            pBLEScan->start(SCAN_PERIOD, true);
+          }
+          else if(major == (3<<8) && minor == 1 && !isPlaying){
+            SpecifyMusicPlay(3);
+            isPlaying = true;
+            startTime = millis();
+          }
+        }
+      }
+      xSemaphoreGive(semaphore);
+    }
+
+    // 3초동안 노래재생 못하게 하기
+    if (isPlaying && (millis() - startTime >= 3000)) {
+      isPlaying = false;
+    }
+
+    vTaskDelay(100 / portTICK_PERIOD_MS);  // Delay for 0.1 second
+  }
+}
+
+void beaconTask(void* pvParameters){
+  for (;;) {
+    if (xSemaphoreTake(semaphore, portMAX_DELAY) == pdTRUE){
+      oBeacon.setManufacturerId(0x4c00);
+      oBeacon.setProximityUUID(BLEUUID(BEACON_UUID));
+      oBeacon.setMajor((2<<8)+1);
+      oBeacon.setMinor(1);
+
+      oAdvertisementData.setFlags(0x04);
+      oAdvertisementData.setManufacturerData(oBeacon.getData());
+
+      pAdvertising->setAdvertisementData(oAdvertisementData);
+      pAdvertising->setScanResponseData(oAdvertisementData);
+      pAdvertising->start();
+
+      BLEScanResults foundDevices = pBLEScan->start(1, false);
+
+      for (int i = 0; i < foundDevices.getCount(); i++) {
+        BLEAdvertisedDevice advertisedDevice = foundDevices.getDevice(i);
+        callbacks.onResult(advertisedDevice);
+
+        std::string manufacturerData = advertisedDevice.getManufacturerData();
+        if (manufacturerData.length() >= 24) {
+          //Serial.println("원하는거 찾음");
+          //신호값 받기
+          uint16_t major = manufacturerData[20] << 8 | manufacturerData[21];
+          uint16_t minor = manufacturerData[22] << 8 | manufacturerData[23];
+          
+          if(major == (3<<8) && minor == 1){
+            oBeacon.setMajor((4<<8)+1);
+            oBeacon.setMinor(1);
+            oAdvertisementData.setManufacturerData(oBeacon.getData());
+            pAdvertising->setAdvertisementData(oAdvertisementData);
+            pAdvertising->setScanResponseData(oAdvertisementData);
+          }
+        }
+      }
+      pAdvertising->start();
+      xSemaphoreGive(semaphore);
+    }
+    vTaskDelay(100 / portTICK_PERIOD_MS);  // Delay for 0.1 second
+  }
+}
 
 void setup() {
+  BLEDevice::init("RIVO_iBeacon");
+  //Serial.begin(115200);
+
+  semaphore = xSemaphoreCreateMutex();
+
+  //---------------------mp3 구성-------------------
+  mp3.begin(9600);
+  delay(100);
+  
+  SelectPlayerDevice(0x02);       // Select SD card as the player device. 내 디바이스에 있는 SD카드에서 파일 가져오기
+  SetVolume(0x1E);        //볼륨설정하기 최대 0x1E
+  //-------------------------------------------------
+
+  pBLEScan = BLEDevice::getScan();
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);
+
+  // Create a task for scanning
   xTaskCreatePinnedToCore(scanTask, "Scan Task", 10000, NULL, 1, &scanTaskHandle, 0); // Scan Task 생성
   xTaskCreatePinnedToCore(beaconTask, "Beacon Task", 10000, NULL, 2, &beaconTaskHandle, 1); // Beacon Task 생성
 }
 
 void loop() {
-  // 아무 작업도 수행하지 않음
-  vTaskDelay(1000 / portTICK_PERIOD_MS); // 1초 대기
-}
-
-void scanTask(void *pvParameters) {
-  BLEScan* pBLEScan = BLEDevice::getScan(); // 스캔 객체 생성
-  pBLEScan->setInterval(SCAN_INTERVAL);
-  pBLEScan->setWindow(SCAN_INTERVAL - 1);
-  pBLEScan->setActiveScan(true); // 활성화된 스캔
-
-  while (1) {
-    BLEScanResults foundDevices = pBLEScan->start(SCAN_PERIOD, false); // 스캔 시작
-    // 스캔 결과 처리
-
-    for (int i = 0; i < foundDevices.getCount(); i++) {
-      BLEAdvertisedDevice device = foundDevices.getDevice(i);
-    }
-
-    vTaskDelay(1000 / portTICK_PERIOD_MS); // 1초 대기
-  }
-}
-
-void beaconTask(void *pvParameters) {
-  while (1) {
-    // 비콘 작업 수행
-    vTaskDelay(1000 / portTICK_PERIOD_MS); // 1초 대기
-  }
+  vTaskDelay(100 / portTICK_PERIOD_MS); // 0.1초 대기
 }
--- code/Kiosk/Kiosk.ino
+++ code/Kiosk/Kiosk.ino
@@ -4,140 +4,156 @@
 #include "BLEAdvertisedDevice.h"
 #include "BLEBeacon.h"
 #include "esp_sleep.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-//---------------------------------------------------------------kiosk 코드---------------------------------------------------------------
-
-// iBeacon UUID
+// UUID
 #define BEACON_UUID "8ec76ea3-6668-48da-9866-75be8bc86f4d"
 #define TEST_BEACON_UUID "39ED98FF-2900-441A-802F-9C398FC199D2"
 
 // 스캔 주기 및 스캔 시간
 #define SCAN_INTERVAL 100
-const int SCAN_PERIOD = 10000; // 스캔 주기 (ms)
+#define SCAN_PERIOD 10000
 
 //오디오
-#include <SoftwareSerial.h>
-#include <MP3Player_KT403A.h>
+#include "SoftwareSerial.h"
+#include "MP3Player_KT403A.h"
 SoftwareSerial mp3(17, 16); //TX, RX  (GIOP 번호)
 
-BLEAdvertising *pAdvertising;   //송출 포인터설정
-BLEScan* pBLEScan;   //스캔포인터설정
+bool playsong = true;
 
-//비콘 manufacturerr data 로 감지하는 클래스
-class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
-  private:
-    BLEBeacon oBeacon;
-    BLEAdvertisementData oAdvertisementData;
+BLEScan* pBLEScan;
+BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+BLEBeacon oBeacon = BLEBeacon();
+BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
 
+SemaphoreHandle_t semaphore;
+TaskHandle_t scanTaskHandle;
+TaskHandle_t beaconTaskHandle;
+
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
   public:
-    MyAdvertisedDeviceCallbacks() {
-      // 객체 초기화
-      oBeacon = BLEBeacon();
-      oAdvertisementData = BLEAdvertisementData();
-      pAdvertising = BLEDevice::getAdvertising();
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    std::string manufacturerData = advertisedDevice.getManufacturerData();
+
+    if (advertisedDevice.haveManufacturerData() && manufacturerData.length() == 25 &&
+        manufacturerData[0] == 0x4c && manufacturerData[1] == 0x00 &&
+        manufacturerData[2] == 0x02 && manufacturerData[3] == 0x15 &&
+        manufacturerData[4] == 0x39 && manufacturerData[5] == 0xed &&
+        manufacturerData[6] == 0x98 && manufacturerData[7] == 0xff &&
+        manufacturerData[8] == 0x29 && manufacturerData[9] == 0x00) {
+      
     }
+  }
+};
 
-    void onResult(BLEAdvertisedDevice advertisedDevice) {
-      Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+MyAdvertisedDeviceCallbacks callbacks;
 
-      // iBeacon 감지 (manufacturerdata가 있는지 확인하고 iBeacon 패킷 구성에 맞는 manufacturerData인지 확인하기)
-      if (advertisedDevice.haveManufacturerData() && advertisedDevice.getManufacturerData().length() == 25 &&
-          advertisedDevice.getManufacturerData()[0] == 0x4c && advertisedDevice.getManufacturerData()[1] == 0x00 &&
-          advertisedDevice.getManufacturerData()[2] == 0x02 &&  advertisedDevice.getManufacturerData()[3] == 0x15 &&   //여기까지는 아이비콘 기본구성
-          advertisedDevice.getManufacturerData()[4] == 0x39 &&  advertisedDevice.getManufacturerData()[5] == 0xed &&   //여기부터 uuid
-          advertisedDevice.getManufacturerData()[6] == 0x98 &&  advertisedDevice.getManufacturerData()[7] == 0xff &&
-          advertisedDevice.getManufacturerData()[8] == 0x29 &&  advertisedDevice.getManufacturerData()[9] == 0x00 ){
-            /*
-          advertisedDevice.getManufacturerData()[10] == 0x44 &&  advertisedDevice.getManufacturerData()[11] == 0x1a &&
-          advertisedDevice.getManufacturerData()[12] == 0x80 &&  advertisedDevice.getManufacturerData()[13] == 0x2f &&
-          advertisedDevice.getManufacturerData()[14] == 0x9c &&  advertisedDevice.getManufacturerData()[15] == 0x39 &&
-          advertisedDevice.getManufacturerData()[16] == 0x8f &&  advertisedDevice.getManufacturerData()[17] == 0xc1 &&
-          advertisedDevice.getManufacturerData()[18] == 0x99 &&  advertisedDevice.getManufacturerData()[19] == 0xd2) {
-            */
-        
-        //Serial.println("지정된 iBeacon 탐색됨.");
+void scanTask(void* pvParameters){
+  bool isPlaying = false;
+  unsigned long startTime = 0;
 
-        //비콘 찾은 후 신호 방출
-        oBeacon.setManufacturerId(0x4c00);   //company ID
-        oBeacon.setProximityUUID(BLEUUID(BEACON_UUID));    //UUID
-        oBeacon.setMajor((2<<8)+1);  // 0000 0002 0000 0001  액션, 그룹, ID, 셀렉트
-        oBeacon.setMinor(1);
+  for (;;) {
+    if (xSemaphoreTake(semaphore, portMAX_DELAY) == pdTRUE) {
+      BLEScanResults foundDevices = pBLEScan->start(1, false);
+      for (int i = 0; i < foundDevices.getCount(); i++) {
+        BLEAdvertisedDevice advertisedDevice = foundDevices.getDevice(i);
+        callbacks.onResult(advertisedDevice);
 
-        oAdvertisementData.setFlags(0x04);
-        oAdvertisementData.setManufacturerData(oBeacon.getData());
-
-        pAdvertising->setAdvertisementData(oAdvertisementData);
-        pAdvertising->setScanResponseData(oAdvertisementData);
-        pAdvertising->start();
-
-        // major, minor 값 뽑아오기
-        std::string payload = advertisedDevice.getManufacturerData();
-        uint16_t currentMajor = payload[20] << 8 | payload[21];
-        uint16_t currentMinor = payload[22] << 8 | payload[23];
-        //Serial.printf("Major: %d, Minor: %d\n", currentMajor, currentMinor);
-
-        //if (currentMajor != previousMajor || currentMinor != previousMinor) {
-            // 원하는 비콘의 Major, Minor 값이 변경되면 실행할 코드 작성
-            // Major, Minor 값을 업데이트
-            //previousMajor = currentMajor;
-            //previousMinor = currentMinor;
-
-            //Serial.println("비콘의 Major 또는 Minor 값이 변경되었습니다.");
-
-        if (currentMajor == (1<<8) && currentMinor == 1) {
-          //Serial.println("주변 스캔신호~!!");
-          //Serial.println("주변을 스캔합니다.");
-          pBLEScan->start(SCAN_PERIOD, true);
-        }
-
-        else if(currentMajor == (3<<8) && currentMinor == 1){
-
-          //Serial.println("노래 재생신호~!!");
-          //Serial.println("노래를 재생합니다.");
-          //비콘 찾아서 신호 보내주려고 major, minor 값 바꿔서 전송
-
-          oBeacon.setManufacturerId(0x4c00);   //company ID
-          oBeacon.setProximityUUID(BLEUUID(BEACON_UUID));    //UUID
-          oBeacon.setMajor((4<<8)+1);
-          oBeacon.setMinor(1);
-
-          oAdvertisementData.setFlags(0x04);
-          oAdvertisementData.setManufacturerData(oBeacon.getData());
-
-          pAdvertising->setAdvertisementData(oAdvertisementData);
-          pAdvertising->setScanResponseData(oAdvertisementData);
-          pAdvertising->start();
-
-          SpecifyMusicPlay(3);
+        std::string manufacturerData = advertisedDevice.getManufacturerData();
+        if (manufacturerData.length() >= 24) {
+          //신호값 받기
+          
+          uint16_t major = manufacturerData[20] << 8 | manufacturerData[21];
+          uint16_t minor = manufacturerData[22] << 8 | manufacturerData[23];
+          
+          if (major == (1<<8) && minor == 1) {
+            pBLEScan->start(SCAN_PERIOD, true);
+          }
+          else if(major == (3<<8) && minor == 1 && !isPlaying){
+            SpecifyMusicPlay(3);
+            isPlaying = true;
+            startTime = millis();
+          }
         }
       }
+      xSemaphoreGive(semaphore);
     }
-  };
 
-void setup() {
-  //--------------------비콘구성---------------------
-  BLEDevice::init("RIVO_iBeacon");
-  BLEDevice::startAdvertising();
+    // 3초동안 노래재생 못하게 하기
+    if (isPlaying && (millis() - startTime >= 3000)) {
+      isPlaying = false;
+    }
 
-  //---------------------mp3 구성-------------------
-    mp3.begin(9600);
-    delay(100);
-    
-    SelectPlayerDevice(0x02);       // Select SD card as the player device. 내 디바이스에 있는 SD카드에서 파일 가져오기
-    SetVolume(0x1E);        //볼륨설정하기 최대 0x1E
-  //------------------------------------------------
-
-  //------------------비콘검색-----------------------
-  pBLEScan = BLEDevice::getScan(); // 새로운 스캔 객체 생성
-  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->setInterval(SCAN_INTERVAL);
-  pBLEScan->setWindow(SCAN_INTERVAL - 1);
-  pBLEScan->setActiveScan(true); // 활성화된 스캔은 더 많은 전력을 사용하지만 더 빠르게 결과를 얻을 수 있습니다.
-  //-------------------------------------------------
+    vTaskDelay(100 / portTICK_PERIOD_MS);  // 0.1초 대기
+  }
 }
 
+void beaconTask(void* pvParameters){
+  for (;;) {
+    if (xSemaphoreTake(semaphore, portMAX_DELAY) == pdTRUE){
+      oBeacon.setManufacturerId(0x4c00);
+      oBeacon.setProximityUUID(BLEUUID(BEACON_UUID));
+      oBeacon.setMajor((2<<8)+1);
+      oBeacon.setMinor(1);
+
+      oAdvertisementData.setFlags(0x04);
+      oAdvertisementData.setManufacturerData(oBeacon.getData());
+
+      pAdvertising->setAdvertisementData(oAdvertisementData);
+      pAdvertising->setScanResponseData(oAdvertisementData);
+      pAdvertising->start();
+
+      BLEScanResults foundDevices = pBLEScan->start(1, false);
+
+      for (int i = 0; i < foundDevices.getCount(); i++) {
+        BLEAdvertisedDevice advertisedDevice = foundDevices.getDevice(i);
+        callbacks.onResult(advertisedDevice);
+
+        std::string manufacturerData = advertisedDevice.getManufacturerData();
+        if (manufacturerData.length() >= 24) {
+          //신호값 받기
+          uint16_t major = manufacturerData[20] << 8 | manufacturerData[21];
+          uint16_t minor = manufacturerData[22] << 8 | manufacturerData[23];
+          
+          if(major == (3<<8) && minor == 1){
+            oBeacon.setMajor((4<<8)+1);
+            oBeacon.setMinor(1);
+            oAdvertisementData.setManufacturerData(oBeacon.getData());
+            pAdvertising->setAdvertisementData(oAdvertisementData);
+            pAdvertising->setScanResponseData(oAdvertisementData);
+          }
+        }
+      }
+      pAdvertising->start();
+      xSemaphoreGive(semaphore);
+    }
+    vTaskDelay(100 / portTICK_PERIOD_MS);  // 0.1초 대기
+  }
+}
+
+void setup() {
+  BLEDevice::init("RIVO_iBeacon");
+  //Serial.begin(115200);
+
+  semaphore = xSemaphoreCreateMutex();
+
+  //---------------------mp3 구성-------------------
+  mp3.begin(9600);
+  delay(100);
+  
+  SelectPlayerDevice(0x02);       // Select SD card as the player device. 내 디바이스에 있는 SD카드에서 파일 가져오기
+  SetVolume(0x1E);        //볼륨설정하기 최대 0x1E
+  //-------------------------------------------------
+
+  pBLEScan = BLEDevice::getScan();
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);
+
+  xTaskCreatePinnedToCore(scanTask, "Scan Task", 10000, NULL, 1, &scanTaskHandle, 0); // Scan Task 생성
+  xTaskCreatePinnedToCore(beaconTask, "Beacon Task", 10000, NULL, 2, &beaconTaskHandle, 1); // Beacon Task 생성
+}
 
 void loop() {
-  BLEScanResults foundDevices = pBLEScan->start(SCAN_PERIOD, false);
-  //lastSignalTime = millis();  // 타이머 초기화
+  vTaskDelay(100 / portTICK_PERIOD_MS); // 0.1초 대기
 }
